43ef6adbd09674b1fff45c23ee7240076d8f895f
Pau <paucas01@alu.iesserpis.org>
2021-05-05 17:46:08 +0200

    ·

--- src/Api.cs
+++ src/Api.cs
@@ -96,9 +96,9 @@ namespace DocumentPlagiarismChecker
         /// </summary>
         /// <param name="results">A set of file matching scores</param>
         public void WriteOutput(){
-            //TODsdfsdfa<ZZZ
-            TerminalOutput t = new TerminalOutput(this.Settings);//dsfKEEE
-            t.Write(this.MatchingResults);//dfngidbfgubdfjhbdfghjbdfhjgb
+            
+            TerminalOutput t = new TerminalOutput(this.Settings);
+            t.Write(this.MatchingResults);//dfnbdfghjbdfhjgb
         }
 
         /// <summary>
